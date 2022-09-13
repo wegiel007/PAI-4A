7d9bd81507288d84b8edb7eab1f6dbc6d172d2cd
wegiel007 <wegiel007@gmail.com>
2022-09-13 11:06:37 +0200

    aha

---
+++ Lekcja 1 wprowadzzeni/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Gud stronica</title>
+	</head>
+	<body>
+
+		<center>Lubie pierogi</center>
+	<?php
+		$zmienna1 = "My Counter 1";
+		echo "hello world","<br>". $zmienna1. "<br>","Hello Friend";
+
+	?>
+	</body>
+</html>
